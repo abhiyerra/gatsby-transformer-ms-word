294ef652b6c53a367655de0a2ecd101690977d2b
Abhi Yerra <abhi@berkeley.edu>
2019-08-01 10:47:50 -0700

    Added code

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,49 +1,54 @@
-const Promise = require(`bluebird`)
-const PDFParser = require(`pdf2json`)
+const mammoth = require("mammoth")
+const getDocumentProperties = require('office-document-properties');
 
-const convertToJson = path =>
-  new Promise((res, rej) => {
-    const pdfParser = new PDFParser(this, 1)
-    pdfParser.loadPDF(path)
-    pdfParser
-      .on(`pdfParser_dataReady`, pdfData => {
-        res(pdfParser.getRawTextContent())
-      })
-      .on(`pdfParser_dataError`, errData => {
-        rej(`PDF to JSON conversion failed!`)
-      })
+
+const convertToJson = path => new Promise((res, rej) => {
+  mammoth.convertToHtml({ path: path }, {})
+    .then(result => res(result.value))
+    .done()
+})
+
+const convertToMetadata = path => new Promise((res, rej) => {
+  getDocumentProperties.fromFilePath(path, function (err, data) {
+    res(data)
+    if (err) {
+      rej(err)
+    }
   })
+})
 
 async function onCreateNode({
   node,
   actions,
-  loadNodeContent,
   createNodeId,
   createContentDigest,
 }) {
   const { createNode, createParentChildLink } = actions
 
   // Filter out non-pdf content
-  if (node.extension !== `pdf`) {
+  if (node.extension !== `docx`) {
     return
   }
 
   let parsedContent = await convertToJson(node.absolutePath)
+  let parsedMetadata = await convertToMetadata(node.absolutePath)
 
-  const pdfNode = {
+  const docxNode = {
     id: createNodeId(`${node.id} >>> ${node.extension}`),
     children: [],
     parent: node.id,
+    name: node.name,
     internal: {
-      type: `pdf`,
+      type: `docx`,
     },
   }
 
-  pdfNode.content = parsedContent
-  pdfNode.internal.contentDigest = createContentDigest(pdfNode)
+  docxNode.content = parsedContent
+  docxNode.metadata = parsedMetadata
+  docxNode.internal.contentDigest = createContentDigest(docxNode)
 
-  createNode(pdfNode)
-  createParentChildLink({ parent: node, child: pdfNode })
+  createNode(docxNode)
+  createParentChildLink({ parent: node, child: docxNode })
 }
 
 exports.onCreateNode = onCreateNode
